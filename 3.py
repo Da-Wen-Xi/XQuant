2db6f784faed8f4d9a037a0b5d1c7cac5be5a8c0
xxt <553624797@qq.com>
2026-02-24 18:32:12 +0800

    注销代理

--- 3.py
+++ 3.py
@@ -4,8 +4,8 @@ import akshare_proxy_patch
 
 
 
-os.environ["http_proxy"] = "http://127.0.0.1:7897"
-os.environ["https_proxy"] = "http://127.0.0.1:7897"
+# os.environ["http_proxy"] = "http://127.0.0.1:7897"
+# os.environ["https_proxy"] = "http://127.0.0.1:7897"
 if __name__ == '__main__':
     akshare_proxy_patch.install_patch("101.201.173.125", "", 30)
     stock_zh_a_hist_df = ak.stock_zh_a_hist(symbol="601700", period="daily", start_date="20240301", end_date='20240528',
